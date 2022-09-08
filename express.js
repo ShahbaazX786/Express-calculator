28cdd172b76f93af3589620dc5d286c0277c2b5a
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-08 12:25:29 +0530

    1.03-Added post method, installed bodyparser

--- express.js
+++ express.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/index.html'); // __dirname returns the current path even on server or local computer
-})
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/index.html"); // __dirname returns the current path even on server or local computer
+});
+
+
+app.post('/',(req,res)=>{
+    // console.log(req.body);
+    var n1=Number(req.body.num1);
+    var n2=Number(req.body.num2);
+    var result=n1+n2;
+    res.send("your result:"+ result);
+});
 
 app.listen(3000,function(){
     console.log("server started at port 3000 bruh");
-})
+});
