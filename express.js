9d999b920d7d957399137ada192f3754be8c9258
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-08 12:07:17 +0530

    1.02-Added a html file and get function in js

--- express.js
+++ express.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html'); // __dirname returns the current path even on server or local computer
+})
+
 app.listen(3000,function(){
     console.log("server started at port 3000 bruh");
 })
